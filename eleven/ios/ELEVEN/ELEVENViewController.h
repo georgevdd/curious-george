ee923347966d384b53fa396675ae3dd87f0c60f3
George van den Driessche <gbmvdd@gmail.com>
2014-02-27 21:43:42 +0000

    Added new iOS project with FB libs.

---
+++ eleven/ios/ELEVEN/ELEVENViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELEVENViewController.h
+//  ELEVEN
+//
+//  Created by George van den Driessche on 2/27/14.
+//  Copyright (c) 2014 George van den Driessche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELEVENViewController : UIViewController
+
+@end
---
+++ eleven/ios/ELEVEN/ELEVENViewController.m
@@ -0,0 +1,29 @@
+//
+//  ELEVENViewController.m
+//  ELEVEN
+//
+//  Created by George van den Driessche on 2/27/14.
+//  Copyright (c) 2014 George van den Driessche. All rights reserved.
+//
+
+#import "ELEVENViewController.h"
+
+@interface ELEVENViewController ()
+
+@end
+
+@implementation ELEVENViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
