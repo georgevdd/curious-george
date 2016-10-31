efc1f1b21ed3721aaad136f8aba6442add93cc70
George van den Driessche <georgevdd@google.com>
2016-10-31 00:53:36 +0000

    Tidy

--- Euler.hs
+++ Euler.hs
@@ -12,94 +12,25 @@ import Data.Text as Text (filter, pack, split, unpack)
 
 import Prelude as P
 
-x `divides` y = y `mod` x == 0
-
-fibs = 1 : 1 : map (uncurry (+)) (zip fibs (tail fibs))
-
 isqrt = round . sqrt . fromIntegral
 
--- O(Nsqrt(N))
-primes = 2:3:[x | x <- [5..], not $ any (`divides` x) (takeWhile (<= isqrt x) primes)]
-
-prime_factors 1 = []
-prime_factors n = let m = head [x | x <- primes, x `divides` n] in m : prime_factors (n `div` m)
-
--- Numbers that in base `b` are palindromic and have `n` digits (including
--- those with leading zeros)
-palindromes b 1 = [0..b-1]
-palindromes b 2 = [(b+1) * d | d <- [0..b-1]]
-palindromes b n = let ps = palindromes b (n-2)
-                      x = b^(n-1) + 1
-                      xs = [x * d | d <- [0..b-1]]
-                  in [p*b + x | x <- xs, p <- ps]
-
-x `inbase` b = reverse $ unfoldr (\x -> guard (x/=0) >> Just (x `mod` b, x `div` b)) x
-
---prime_decomposition = map (head &&& length) . group . prime_factors
-
+(.:) = (.) . (.)
 equalBy f x y = (f x) == (f y)
 
+takeWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+takeWhileM pred (x:xs) = do
+  p <- pred x
+  if p
+  then do xs' <- takeWhileM pred xs
+          return (x:xs')
+  else return []
+takeWhileM _ [] = return []
 
--- enumerates all pairs of positive integers, smallest sum first.
-diagonals = concat [[(x, n-x) | x <- [1..n-1]] | n <- [1..]]
 
--- enumates pairs of positive integers where the first is no larger than the
--- second, smallest sum first.
-diagonals' = concat [[(x, n-x) | x <- [1..n `div` 2]] | n <- [1..]]
-
-pythagorean (a, b, c) = a*a + b*b == c*c
-
-triangle n = n * (n+1) `div` 2
-divisors n = [x | x <- [1..n `div` 2], x `divides` n]
---perfect n = sum (divisors n) == n
-abundant n = sum (divisors n) > n
+-- ### Combinatorics ### --
 
 factorial n = foldr1 (*) [1..n]
 n `ncr` r = factorial n `div` (factorial r * factorial (n - r))
-num_divisors = product . map ((+1) . length) . group . prime_factors
-
-collatz_step n | 2 `divides` n = n `div` 2
-               | otherwise = 3*n + 1
-collatz = takeWhile (/=1) . iterate collatz_step
-
-collatz' m n = case M.lookup n m of
-                 Just k -> (m, k)
-                 Nothing ->
-                     let n' = collatz_step n
-                         (m', k') = collatz' m n'
-                     in (M.insert n (k'+1) m', k'+1)
-
-spellNumber n =
-  let
-    (h, t, u) = (n `div` 100, n `div` 10 `mod` 10, n `mod` 10)
-    spell 0 0 u = unitsNames !! (u - 1)
-    spell 0 t 0 = tensNames !! (t - 1)
-    spell 0 t u = if t == 1
-                  then teensNames !! (u - 1)
-                  else spell 0 t 0 ++ "-" ++ spell 0 0 u
-    spell h 0 0 = unitsNames !! (h - 1) ++ " hundred"
-    spell h t u = spell h 0 0 ++ " and " ++ spell 0 t u
-  in if n == 1000 then "one thousand" else spell h t u
-
-triMax = head . foldr1 step
-  where step line1 line2 = [max x y | (x, y) <- zip [a + b  | (a, b ) <- zip line1 line2       ]
-                                                    [a + b' | (a, b') <- zip line1 (tail line2)]]
-
-monthDays year = [
-  31, febDays year, 31,
-  30, 31, 30,
-  31, 31, 30,
-  31, 30, 31]
-febDays year | year `mod` 400 == 0 = 29
-             | year `mod` 100 == 0 = 28
-             | year `mod` 4 == 0 = 29
-             | otherwise = 28
-
-digits = map ((read :: (Integral a, Read a) => String -> a) . return) . show
-fromDigits = foldl (\n d -> 10 * n + d) 0
-
-amicable x = let y = d(x) in y /= x && d(y) == x
-  where d(x) = sum $ divisors x
 
 -- Given a sorted list, produces its permutations in lexicographical order
 lexPerms [] = [[]]
@@ -114,11 +45,150 @@ choices' []     _ = []
 choices' (x:xs) r = [(x:ys, zs) | (ys, zs) <- choices' xs (r-1)] ++
                     [(ys, x:zs) | (ys, zs) <- choices' xs  r   ]
 
-(.:) = (.) . (.)
 choices = map fst .: choices'
 
 partitions [] = [([], [])]
 partitions (x:xs) = concat [[(x:ys, zs), (ys, x:zs)] | (ys, zs) <- partitions xs]
+
+
+-- ### Number theory ### --
+
+x `divides` y = y `mod` x == 0
+num_divisors = product . map ((+1) . length) . group . prime_factors
+divisors n = [x | x <- [1..n `div` 2], x `divides` n]
+perfect n = sum (divisors n) == n
+abundant n = sum (divisors n) > n
+amicable x = let y = d(x) in y /= x && d(y) == x
+  where d(x) = sum $ divisors x
+
+triangle n = n * (n+1) `div` 2
+
+pythagorean (a, b, c) = a*a + b*b == c*c
+
+
+-- ### Prime numbers ### --
+
+-- O(Nsqrt(N))
+primes = 2:3:[x | x <- [5..], not $ any (`divides` x) (takeWhile (<= isqrt x) primes)]
+
+prime_factors 1 = []
+prime_factors n = let m = head [x | x <- primes, x `divides` n] in m : prime_factors (n `div` m)
+
+type Sieve = (S.Set Int, [Int])
+emptySieve :: Sieve
+emptySieve = (S.empty, primes)
+
+testPrime :: Int -> State Sieve Bool
+testPrime x = do
+  (cache, ps) <- get
+  when (x >= head ps)
+       (modify (fillCacheTo x))
+  cc <- gets fst
+  return $ x `S.member` cc
+ where
+  fillCacheTo x (cache, ps) =
+    let (more, ps') = break (>x) ps
+    in (cache `S.union` (S.fromDistinctAscList more), ps')
+
+-- For making a huge prime sieve, with a crude progress counter
+sieveUpTo :: Int -> IO Sieve
+sieveUpTo 0 = return emptySieve
+sieveUpTo n = do
+  s' <- sieveUpTo (n - k)
+  seq (head $ snd s') (putChar '.')
+  return $ execState (mapM testPrime [max 0 (n-k+1)..n]) s'
+ where k = 10^6
+
+
+-- ### Other integer sequences ### --
+
+fibs = 1 : 1 : map (uncurry (+)) (zip fibs (tail fibs))
+
+-- Numbers that in base `b` are palindromic and have `n` digits (including
+-- those with leading zeros)
+palindromes b 1 = [0..b-1]
+palindromes b 2 = [(b+1) * d | d <- [0..b-1]]
+palindromes b n = let ps = palindromes b (n-2)
+                      x = b^(n-1) + 1
+                      xs = [x * d | d <- [0..b-1]]
+                  in [p*b + x | x <- xs, p <- ps]
+
+
+--prime_decomposition = map (head &&& length) . group . prime_factors
+
+
+
+-- enumerates all pairs of positive integers, smallest sum first.
+diagonals = concat [[(x, n-x) | x <- [1..n-1]] | n <- [1..]]
+
+-- enumates pairs of positive integers where the first is no larger than the
+-- second, smallest sum first.
+diagonals' = concat [[(x, n-x) | x <- [1..n `div` 2]] | n <- [1..]]
+
+
+-- ### Collatz conjecture ### --
+
+collatz_step n | 2 `divides` n = n `div` 2
+               | otherwise = 3*n + 1
+collatz = takeWhile (/=1) . iterate collatz_step
+
+collatz' m n = case M.lookup n m of
+                 Just k -> (m, k)
+                 Nothing ->
+                     let n' = collatz_step n
+                         (m', k') = collatz' m n'
+                     in (M.insert n (k'+1) m', k'+1)
+
+triMax = head . foldr1 step
+  where step line1 line2 = [max x y | (x, y) <- zip [a + b  | (a, b ) <- zip line1 line2       ]
+                                                    [a + b' | (a, b') <- zip line1 (tail line2)]]
+
+
+-- ### Written representations ### --
+
+digits = map ((read :: (Integral a, Read a) => String -> a) . return) . show
+fromDigits = foldl (\n d -> 10 * n + d) 0
+
+x `inbase` b = reverse $ unfoldr (\x -> guard (x/=0) >> Just (x `mod` b, x `div` b)) x
+
+spellNumber n =
+  let
+    (h, t, u) = (n `div` 100, n `div` 10 `mod` 10, n `mod` 10)
+    spell 0 0 u = unitsNames !! (u - 1)
+    spell 0 t 0 = tensNames !! (t - 1)
+    spell 0 t u = if t == 1
+                  then teensNames !! (u - 1)
+                  else spell 0 t 0 ++ "-" ++ spell 0 0 u
+    spell h 0 0 = unitsNames !! (h - 1) ++ " hundred"
+    spell h t u = spell h 0 0 ++ " and " ++ spell 0 t u
+  in if n == 1000 then "one thousand" else spell h t u
+
+
+-- ### Date and time ### --
+
+monthDays year = [
+  31, febDays year, 31,
+  30, 31, 30,
+  31, 31, 30,
+  31, 30, 31]
+febDays year | year `mod` 400 == 0 = 29
+             | year `mod` 100 == 0 = 28
+             | year `mod` 4 == 0 = 29
+             | otherwise = 28
+
+
+-- ### Currency ### --
+changes :: Int -> Int -> [[Int]]
+changes m p = do
+  coin <- denoms
+  if coin > p || coin > m then []
+  else if coin == p then [[coin]]
+       else [(coin:rest) | rest <- changes coin (p-coin)]
+ where
+  denoms = reverse [1, 2, 5, 10, 20, 50, 100, 200]
+
+
+-- ### Problems ### --
 
 euler1 = sum [x | x <- [1..999], 3 `divides` x || 5 `divides` x]
 euler2 = sum [x | x <- takeWhile (< 4000000) fibs, 2 `divides` x]
@@ -182,32 +252,6 @@ euler26 =
 -- euler27
 -- b must be prime otherwise n=0 is composite
 -- a+b must be even otherwise n=1 == 1+a+b is composite
-
-type Sieve = (S.Set Int, [Int])
-emptySieve :: Sieve
-emptySieve = (S.empty, primes)
-
-testPrime :: Int -> State Sieve Bool
-testPrime x = do
-  (cache, ps) <- get
-  when (x >= head ps)
-       (modify (fillCacheTo x))
-  cc <- gets fst
-  return $ x `S.member` cc
- where
-  fillCacheTo x (cache, ps) =
-    let (more, ps') = break (>x) ps
-    in (cache `S.union` (S.fromDistinctAscList more), ps')
-
-takeWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
-takeWhileM pred (x:xs) = do
-  p <- pred x
-  if p
-  then do xs' <- takeWhileM pred xs
-          return (x:xs')
-  else return []
-takeWhileM _ [] = return []
-
 euler27 = let (a, b, _) = bestTriple in a*b
  where
   quadratic a b n = n*n + a*n + b
@@ -227,15 +271,6 @@ euler27 = let (a, b, _) = bestTriple in a*b
 euler28 = 1 + 4 * sum [4*k*k + k + 1 | k <- [1..500]]
 euler29 = S.size $ foldr1 S.union [S.fromList [a^b | b <- [2..100]] | a <- [2..100]]
 euler30 = sum [x | x <- [1..100000], sum [d^4 | d <- digits x] == x] - 1
-
-changes :: Int -> Int -> [[Int]]
-changes m p = do
-  coin <- denoms
-  if coin > p || coin > m then []
-  else if coin == p then [[coin]]
-       else [(coin:rest) | rest <- changes coin (p-coin)]
- where
-  denoms = reverse [1, 2, 5, 10, 20, 50, 100, 200]
 
 euler31 = length $ changes 200 200
 
@@ -283,6 +318,7 @@ euler39 = (fst . head . head .
   triples = P.filter pythagorean . takeWhile ((<1000) . sum3) $ map triple diagonals'
   triple (a, b) = (a, b, isqrt (a*a + b*b))
   sum3 (a, b, c) = a+b+c
+
 euler40 = product [champernowneDigit $ 10^x-1 | x <- [0..6]]
  where
   -- Compute d'th digit of Champernowne's number
@@ -303,14 +339,6 @@ euler40 = product [champernowneDigit $ 10^x-1 | x <- [0..6]]
   segDigits s = (s+1) * segNums s
   -- Compute segment and digit within segment.
   segRem d = last . takeWhile ((>=0) . snd) . zip [0..] $ scanl (-) d $ map segDigits [0..]
-
-sieveUpTo :: Int -> IO Sieve
-sieveUpTo 0 = return emptySieve
-sieveUpTo n = do
-  s' <- sieveUpTo (n - k)
-  seq (head $ snd s') (putChar '.')
-  return $ execState (mapM testPrime [max 0 (n-k+1)..n]) s'
- where k = 10^6
 
 euler41 = head $ evalState (filterM testPrime ps) emptySieve
  where
