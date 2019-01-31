3d803fe00322ba05c60c6d0b89af5836789c7243
George van den Driessche <georgevdd@georgevdd-macbookpro.roam.corp.google.com>
2019-01-31 00:37:12 +0000

    Tidy some more.

--- euler/Euler61.hs
+++ euler/Euler61.hs
@@ -2,7 +2,7 @@ module Euler61 where
 
 import Control.Monad (msum)
 import Data.List (permutations)
-import Prelude as P
+import Prelude
 import Lib
 
 figurates = [
@@ -19,21 +19,20 @@ ranges = map range $ reverse figurates
 -- cd: first element of each result must begin with this
 -- ab: last element of each result must end with this
 search :: [[Integer]] -> Integer -> Integer -> [[Integer]]
+search (ns:nss) cd ab = do
+  n <- filter (\n -> n `div` 100 == cd) ns
+  [n:rest | rest <- search nss (n `mod` 100) ab]
 search [] cd ab = if cd == ab then [[]] else []
-search nss cd ab = do
-  (ns:nss') <- permutations nss
-  n <- P.filter (\n -> n `div` 100 == cd) ns
-  rest <- search nss' (n `mod` 100) ab
-  return $ n:rest
 
 search2 :: [[Integer]] -> Integer -> [[Integer]]
-search2 nss n =
+search2 nss n = do
   let (ab, cd) = n `divMod` 100
-  in [n:rest | rest <- search nss cd ab]
+  nss' <- permutations nss
+  [n:rest | rest <- search nss' cd ab]
 
 search3 :: [[Integer]] -> [[Integer]]
-search3 nss = head $ filter (not . null) [search2 (tail nss) n | n <- head nss]
+search3 (ns:nss) = concat [search2 nss n | n <- ns]
 
 euler61 =
   let solution = head $ search3 ranges
-  in sum solution
+  in print $ sum solution
