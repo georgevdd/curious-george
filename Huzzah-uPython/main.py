d4a958490f2bd95073d029c32a5a31d7e4862bf4
George van den Driessche <gbmvdd@gmail.com>
2021-04-21 14:58:30 +0100

    Add test of light-blinking coroutine.

--- Huzzah-uPython/main.py
+++ Huzzah-uPython/main.py
@@ -1,2 +1,18 @@
 import esp8266
+
+
+import uasyncio
+
+async def blink(led, period_ms):
+    while True:
+        led.on()
+        await uasyncio.sleep_ms(period_ms)
+        led.off()
+        await uasyncio.sleep_ms(period_ms)
+
+
+import machine
+
+uasyncio.create_task(blink(machine.Pin(0, machine.Pin.OUT), 700))
+
 esp8266.run()
