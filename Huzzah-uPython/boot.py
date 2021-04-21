c8396cc88e06d26fed9afd9a99620858a3d6a1ec
George van den Driessche <gbmvdd@gmail.com>
2021-04-21 14:03:19 +0100

    Add automatic WiFi connection.

--- Huzzah-uPython/boot.py
+++ Huzzah-uPython/boot.py
@@ -7,3 +7,18 @@ import gc
 #import webrepl
 #webrepl.start()
 gc.collect()
+
+import credentials
+
+def do_connect():
+  import network
+  sta_if = network.WLAN(network.STA_IF)
+  if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect(credentials.ssid, credentials.password)
+    while not sta_if.isconnected():
+      pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
