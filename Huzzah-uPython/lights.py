751ba6353f08c9c7c1a99d4b16b2d6d346958d5b
George van den Driessche <gbmvdd@gmail.com>
2021-04-26 00:29:40 +0100

    Basic docs and better formatting for Region.

--- Huzzah-uPython/lights.py
+++ Huzzah-uPython/lights.py
@@ -18,6 +18,11 @@ SHELVES = [13, 30, 47, 67, 88]
 
 
 class Region:
+  """An indexable sequence of pixels in a single strip.
+
+  See https://docs.micropython.org/en/latest/genrst/builtin_types.html
+  for some limitations of slicing in MicroPython.
+  """
 
   def __init__(self, strip, indices):
     self.strip = strip
@@ -25,14 +30,22 @@ class Region:
 
   def __getitem__(self, key):
     if isinstance(key, slice):
-      return Region(self.strip, [self.indices[n] for n in range(key.start or 0, key.stop or len(self.indices), key.step or 1)])
+      return Region(self.strip, [
+        self.indices[n] for n in range(key.start or 0,
+                                       key.stop or len(self.indices),
+                                       key.step or 1)])
     else:
-      return self.strip[key]
+      return self.strip[self.indices[key]]
 
   def __setitem__(self, key, value):
     if isinstance(key, slice):
-      for n in range(key.start or 0, key.stop or len(self.indices), key.step or 1):
+      for n in range(key.start or 0,
+                     key.stop or len(self.indices),
+                     key.step or 1):
         self.strip[self.indices[n]] = value
+    else:
+      self.strip[self.indices[key]] = value
+
 
 TOP = Region(strips[1], range(N_TOP))
 LEFT = Region(strips[0], range(N_SIDE))
@@ -46,6 +59,7 @@ def stop(frame):
 
 def test_regions(frame):
   TOP[:] = (0, 0, 0, 10)
+  TOP[0] = TOP[-1] = (0, 0, 80, 0)
   LEFT[:] = (20, 0, 0, 0)
   RIGHT[:] = (0, 20, 0, 0)
 
